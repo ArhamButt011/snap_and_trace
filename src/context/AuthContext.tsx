5f8005f377fee59d45e17264b86c2c15e38835a3
Arham utt011 <arhamhamidbutt@gmail.com>
2025-04-22 10:07:16 +0500

    Create build

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -30,7 +30,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined)
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext)
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider     ')
+    throw new Error('useAuth must be used within an AuthProvider')
   }
   return context
 }
